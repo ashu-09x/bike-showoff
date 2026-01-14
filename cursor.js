5913dda0f7cae330c8d4f1ff2bf86fa8a9b3e8e1
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-01-14 21:40:55 +0530

    fixed bugs of page1 cursor

--- cursor.js
+++ cursor.js
@@ -1,28 +1,37 @@
-let cursor = document.querySelector(".cursor")
-let page1 = document.querySelector(".page1")
-let img = document.querySelector("img")
-let parentdiv = document.querySelector(".parent")
-page1.addEventListener("mousemove",function(el){
-    let xaxis = el.x
-    let yaxis = el.y
-    gsap.to(cursor,{
-        x:xaxis,
-        y:yaxis,
-        duration:1,
-        ease:"back.out"
+const page1 = document.querySelector(".page1");
+const cursor = document.querySelector(".cursor");
+const parentdiv = document.querySelector(".parent");
 
-    })
-})
+if (page1) {
+  page1.addEventListener("mouseenter", () => {
+    document.body.classList.add("show-cursor");
+  });
 
-parentdiv.addEventListener("mouseenter",function(){
-    gsap.to(cursor,{
-        scale:2,
-        
-    })
-})
+  page1.addEventListener("mouseleave", () => {
+    document.body.classList.remove("show-cursor");
+  });
 
-parentdiv.addEventListener("mouseleave",function(){
-    gsap.to(cursor,{
-        scale:1
-    })
-})
+  page1.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.6,
+      ease: "back.out",
+    });
+  });
+
+  if (parentdiv) {
+    parentdiv.addEventListener("mouseenter", () => {
+      gsap.to(cursor, {
+        scale: 2,
+        backgroundColor: "#5e5353a6",
+      });
+    });
+
+    parentdiv.addEventListener("mouseleave", () => {
+      gsap.to(cursor, {
+        scale: 1,
+      });
+    });
+  }
+}
